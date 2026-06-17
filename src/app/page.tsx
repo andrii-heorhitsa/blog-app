4eb60d3dab7418ec00bdf77f3bd4acde6884895c
Andrii Heorhitsa <georgica.andrii@gmail.com>
2026-06-17 22:57:04 +0300

    chore: optimize main page structure and layout css rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h2>Some Test Title</h2>
+    <div className={styles.main}>
+      <main className={styles.container}>
+        <h2>Dev.to Tech Blog</h2>
       </main>
     </div>
   );
