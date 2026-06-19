78979cf4122e00e34ef9f83ff0f55b3be0a07015
Andrii Heorhitsa <georgica.andrii@gmail.com>
2026-06-19 18:21:20 +0300

    chore: add dev.to remotePatterns to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.dev.to", // Дозволяє будь-які піддомени dev.to
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", // Часто використовується авторами на dev.to
+      },
+    ],
+  },
 };
 
 export default nextConfig;
