6df784a1164923d612a98ce1dc26926b38df9bb7
Andrii Heorhitsa <georgica.andrii@gmail.com>
2026-07-08 12:54:07 +0300

    feat: integrate bookmark count and hydration skeleton into Header

--- src/features/layout/header/Header.view.tsx
+++ src/features/layout/header/Header.view.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import { useBookmarkStore } from "@/store/use-bookmark-store";
 import styles from "./Header.module.css";
+import { Title } from "@/components/title";
+import useHasHydrated from "@/hooks/use-has-hydrated";
+import Skeleton from "@/components/skeleton";
 
 export function Header() {
+  const hasHydrated = useHasHydrated();
+  const bookmarksCount = useBookmarkStore(
+    (state) => state.bookmarkedIds.length,
+  );
+
   return (
     <header className={styles.header}>
       <span>🚀 Dev.to Reader</span>
+      <Title as="h4">
+        {hasHydrated ? (
+          `Bookmarks quantity: ${bookmarksCount}`
+        ) : (
+          <Skeleton width={180} height={24} />
+        )}
+      </Title>
     </header>
   );
 }
